92e13afaaa21e3dbeb45756237f959e2362aa0f5
Çiğdem Türkmen <cigdemturkmen57@outlook.com>
2021-11-04 14:22:54 +0300

    .

--- Northwind.Web/Controllers/AuthController.cs
+++ Northwind.Web/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Northwind.Web.Models.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,13 @@ namespace Northwind.Web.Controllers
     {
         
         public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] // !!!!!!!!!!!!!!
+        public ActionResult Register(RegisterViewModel model)
         {
             return View();
         }
